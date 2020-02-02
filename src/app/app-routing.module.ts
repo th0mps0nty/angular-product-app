a19934b1f87f4e1290ebc06f975abeaf0ab1052e
Tyler <th0mps0n.ty@protonmail.com>
2020-02-01 19:40:45 -0500

    super basic angular app with product list, from angular docs

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
